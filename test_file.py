de78f1751d780018b60edad1cef88b94cb6082b1
Gergely <gergely@cx-ray.com>
2017-04-28 20:29:45 +0200

    nx bohockódás

--- test_file.py
+++ test_file.py
@@ -1,3 +1,19 @@
 __author__ = 'Gergely'
 
-#whatever
+import networkx as nx
+
+nodes = [1, 2, 3, 4, 5, 6, 7]
+
+senders = [1, 2, 2, 3, 4, 5]
+receivers = [2, 1, 3, 4, 2, 1]
+
+graph = nx.Graph()
+
+edges = [(senders[i], receivers[i], {'color': 1}) for i in range(len(senders))]
+
+graph.add_edges_from(edges)
+graph.add_nodes_from(nodes)
+
+print(list(graph.edges()))
+
+print(nx.algorithms.clustering(graph))
